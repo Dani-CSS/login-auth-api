0ed3972a36bfbdbd546ff9353977c1d6a73bb980
Dani-CSS <danielecs26@gmail.com>
2025-10-06 15:07:06 -0300

    initial: add existing backend codebase (Sprint boot, JWT auth, Spring security setup)

---
+++ src/main/java/com/daniele/login_auth_api/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.daniele.login_auth_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+
+}
